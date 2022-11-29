8967bac9113f6a86b5dcadbad573aae4cd75d5b3
mka-Mikhail <mixailrojnow@yandex.ru>
2022-11-29 23:05:26 +0300

    HW7

---
+++ src/main/java/com/mka/springdatajpa/models/Product.java
@@ -0,0 +1,18 @@
+package com.mka.springdatajpa.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "cost")
+    private int cost;
+}
---
+++ src/main/java/com/mka/springdatajpa/services/ProductService.java
@@ -0,0 +1,39 @@
+package com.mka.springdatajpa.services;
+
+import com.mka.springdatajpa.models.Product;
+import com.mka.springdatajpa.models.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getProducts() {
+        Iterable<Product> source = productRepository.findAll();
+        List<Product> products =  new ArrayList<>();
+        source.forEach(products::add);
+        return products;
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElseThrow();
+    }
+
+    public void addNewProductToRepo(Product product) {
+        productRepository.save(product);
+    }
+
+    public void deleteProductFromRepoById(Long id) {
+        productRepository.deleteById(id);
+    }
+}
